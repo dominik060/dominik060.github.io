f22824a78edd66bb25982f94469c4af0199e2240
dominik060 <71775710+dominik060@users.noreply.github.com>
2022-06-05 14:57:39 +0200

    updated navbar

---
+++ main/script.js
@@ -0,0 +1,28 @@
+const themeButton = document.getElementById("themeButton");
+let theme = localStorage.getItem('theme');
+
+const enableLightTheme=()=>{
+    document.body.classList.add("light-theme");
+    localStorage.setItem('theme', 'light');
+}
+
+const disableLightTheme=()=>{
+    document.body.classList.remove("light-theme");
+    localStorage.setItem('theme', null);
+}
+
+if(theme=="light"){
+    enableLightTheme();
+}
+
+
+themeButton.addEventListener("click", ()=>{
+    theme = localStorage.getItem("theme");
+    if(theme!="light"){
+        enableLightTheme();
+    }
+    else{
+        disableLightTheme();
+    }
+});
+
