d2813166183dbd15dfa117149f205f2ced30102f
dominik060 <71775710+dominik060@users.noreply.github.com>
2022-07-03 21:28:25 +0200

    added wordle

---
+++ wordle/main.js
@@ -0,0 +1,210 @@
+
+const allWordsText = 
+'sferaasiatceckosvetrdrnaksaveclesakpornonadrzzaveszaverselenzavetnadralehkotloukregalparianadromydlozamisbazendelkalumikmaniksmrstmatkauzitiracnaprizezamerlvountrzbavezenzluvapletigotikvypekvznikxindlspechcesacuhrabpulkarimsapuvabhurkakytkapandabinecsondajidisvarkavitezbezkafjordcecekremenslehasklebzkrutsezambubenkujondikcearzensklephovorsinkabodlolebkasmradhokejforumdzbanberkasvitazlutokuryrguidedelbasyrarsvrabgalonvinylbehnastrazkanecjikratatkatatiktendrstransterkkyrystepnapajzlpulecidylaemocebrizasimkazavejburzadovozstazeglosadavnobarkahorkodemprrelaxpecarsiruptesilhorkadogmarecesujezduvrathobojfloraodvarstadofakirsponavazkanorekodvalsmyslkraulnovicvodkatelkahonecpracevecnolodkamickapintahlinavackastvolmladistropkankamladepadlorusinjitrostrompolirpizzabaretdilecsuknolisejrehotrukavkovarsuknedilekswingsifonsyselpokecbeckosankybarelbeckavudceokrinhoreckukantydenmackazvestaliasrefyzmicekzimaknasypkulkaoraniternochrupjarmomaniemalbagaucodrozdcizekoranzvyvodvideosenikpohanhrdlojelentrampdurumpsounscenanugathuliczadaktremaviolabidlotrempnapisrosolfelakbukvapotizdebutlazardoletoblakzranikerikaromamotylpatroketasmilarhovnoodvrhvozikzatkafenkahrivagolempalascinelmotellemradebilpalactlamamazutmasoxopratnouzecivkafenylvykyvzabakoblatslohaverzekojnanavinlesbazdivooptikozubirolkazilkachladbavicpasakdrzakpencechlapporizotvortokensobeckliseksefttorzeubrustorzovpustpekansmenapisentangoodporlumeksidlokravasumakzaludsogunnuzkycinkadopallipkazenacpaketzetonstoikotcimlihyrkosaksonetaluzepodeskomikbydlominuskominlilektyckavizkadildoobrokvlcekpylonlepickobkabednaumrtivapnoalibihordabuvoldivakdivanskobabrouksteskodvodhajnyzezloodesametacbajkaalarmmetalrabatmetancuriemetaruhonacislostryclomozmlekoarkusmytuszapisradioloubiradilradicambrakavkatoastvrzotvyrezkutnaledarbarvaburaksefkaobtizaxiomvetryustrkplatoedicevenekbotkakubikvenecdorazmulatslagrsadravyterkazenvztahruinakopracolekjilekrivalkokonjatkajilecsetupoferadozoruhlakhabitparohslechokounlhutalekarverejdrzbaheversortaskluzdlatoetujebyceknerezjedlepampaferblcizbavyzvaodreztkanifabornosickulmamankonosikmottotisichasicumyslgekonhasishumnogenuszroutlogikvikyrbutikjinanbutenskalpstupakyvaltrustetherbingorampaobcanzatezlatrorezbaopichkamoshosiksalonruliknopalblumamasnastenamanezprasecinzetipartunakgeoidorkanvybercetarbrckotrestzakopkrcekpokustreskmlatovybehpopelgarazmyrtavetevvetescidlopasirpoharsplinpazdikotelpasikzemlefotonkotecpekarpychajahenvejirlustrsibalkomzealkenpekacvozkakliprbilekneterhobitautakodtokkapieorechfankaoceanzlotyhrtanhacekborschrichgildavyhentulenjatkyroupyradzabuznatildamufleplastmetylnasupdyadapohonsochavyluhaktinnazornehetaktivponorcejchstenepolomskautosmakpohovposelskaravykopposednacrtznamaboxerznamybandazakonteploustavodpadodpaljasanmlicivrataadrieskicaimiseodparvrhacplicespinamazecrevenlejzrbluzanahonzadrzmazelkasnahumortupechlizazkratobutinavykvikarnarodkabatlampakopacnarokporyvpokrmloknakozkapozoragamanapojviniknekovsecnafesakzbrojvojakzahulsatnaknihapotoktrpytanimevymolrebuszamekatletuchopcivilobilirezeksofersametprcekbodikdubkauvahasamecsekceslukalemplpopissoskaberlatajgaberlesmolapakunpirohcumeldriviparnozatvabudarosvitokresramarmesictovarsprotturindubenjekotulohajavorjeslepenalproudnajempadliaortanaukaperejseslezalivbohaclihennakupstaraskvorbuceksiskasypacodbytnemecsabespanakskrobjuntapomerlucbaexceshribeuchytdizkadepotslinajurtabienaterenrodakhuckadevonpalmauklonfrezaomegastokaobratsigmaobrazvervapoctadirkaobavasackoobradnavesvarlejostarozumosadasirkajmelipazitdatumjogindotekjezidkrizemarkamachrtornamrhacdmutisitkaskvarmachahrbolpeklositkokloubodbojsadbafandaamebavyvarodbodveganksaftcetbashodaodlomfrapeodborhulkarusakkycelcykastikotskorelzicepicusukladrounolimecperutotreskuponpostaotrepbidaktrenitrendnaporradarrentaspuntbaletsipekobtokfikusobzortaborhereckuzleridicpupenpupektykevdendybotelzivotmichazrakytrhakbedlamyvalbehakrajonurodakolielokajprudalokallvicenylonupatilovcikolikzavinbitkakebabosivotisenmodulspalaparmazobanlekcemodusdrdolsyticmobilkonikarkyrdopisbazarbabcakriplsatanetudasluchodmerfikcevidlefinisvecerfutongejsaaltanobnosolivapobytuzinavypisodsunradonreprotranzfaunatranskasararealagensnymfatirazagenttopictrumfcepinorgiehadkastolaberantirakemisesalatvzletjidloosobatrotlsilonvitacopiatsjezdsrdcedlahamockaskladselkaclonamnichsejkrsklarkatarautorbetonpepkauterymaslomuziktapasmaslemangodzberpalbakatankulakkupecmagorvylezjunakmajkakanyrkvadrorteltaviclyrikcisarformakrestvylevpytelmetlarondoposuvsatyrkacerfotaktiaratoxinobdivpojemtonikflinksabatmarzefenolkibucprejtprimazmenavarankvititropyknizemletioperakupkaflexazajicflexekapkajizbastrojmamontichocitathymnakalonpokercitacvytahbitvapivarluzkocumilhonicmoulaalbumkanontypekzaborzinkakanoekolnarahnosopkadredyvztekstelatitullisarefektbarazpoemavydutvyjevspletbarakrebelvarnalisaksanonprvakalkydhorakzahathoralnasepgustocovidplanedomeknaseknosalcizaktenissalsaviskavalkaradnistvacokruhdraftdytikmosazvilkavdechpelesherdalapkapizdarevuesadkadrevozidkaakteruzliknarazgruntdolarzavanvolacfekalborecvolandilkopegasbozecvinkodelicprastuskoksrpensrpekchlorboulematesreteztalirmotaksportrozenrypalrypakmaltalhanibrlohidealsrafapadrtbernezackavstuppaseksketasotekpagerlososlatexseriehmotakauzaarchadusikbradakorankoralporekrundakorabcakannabehzahonmincefemurmerikzaspihruzamericpetkarezielahevskrinrupierezimhyrilbrigaholbaveslokaplealelapanossavlemozekmasakodlivsvicesurikcesloblbecfusakmasazkosermilecbombaparbabujonborbaramusrolakbisonsonarkocarudobivojnapauzabubakzaparjmenijmenozabkaanalyvolnobuketsporarybizmenaztezbachuvapozerrouradeckodiodadeckalabelotlakvousymekacjetelachatgardeladickuklawnd22celbagardasparaadeptmagnamylkalipanovcakbunkazrudanuzakbelicducejsarzedatelrumundrogadamanbratrskutrbolakdivkavenikburenvelurchudyvizazpetitvydajsazectenzeputkaklikamosnatorbaucastkavylamantgestopilotceltakolosofinazacpatrokymysokpatosruzekobsahklaunjiskaradcevlastpinkapocetbrejkodsypcihlamluvariksabalicmedikaprilbalikmrskatafkabijaktaterzkazaneraddalkasektaodhozhvozdcetkapifkavedeczbytifiakrdabertolarodnozsaldodabelromkacapkatrkacostriuradahuhlaakcievkladvytokbunkrzvireakcizdverefoyermodemtilkohlavapijaktanectoporpijansypektopolhadektunkapachtrydlohadecethylroversambaparakboraxparatrovenlarvamodlamestohopiklouzerastrtyfusglumahalirhalitvyzleharasvelindlenidosypedikttablovtiskmalusvydejnaplntitanpasmotarotmagmahackopokynpenisujetisvinekamejlunakkamenmrchamyrhalegatstopasuchohaluzvazalzitnasesitvazaksycaknotorvazacdruidvulvaopiumvokalnaftamixerkurakkuratcevkakaratnecasstanivozbatarasteckakurazkalictulakfousykalifrolbaimageucaristrikdenarstrihmakaksesuvmopedzelvapivotsloupessejkoktafrckadetoxponcoleckapokojzaporstarilianapokosoktansovetstaryikonaremizbundaposypremixnitkabefelchataspiclkacirliscesarinpovelorlikobocicakrakladanotarsekacfakanflirttonervymetvymerumenijazykfialatahacciratpsinahounebluesfazorpiklekrekrcichaidiotduvoddupotdrahaidiomtrasavyhozutvarhlivapustapoctyokapityranzloundriftnazevkolbasafarbalzafetisvlcakslanglaznerutinzakalsickasancepohybvinekmisalmimikcunikvacekorsejsennapesecurbardyskoskryspesekafektgeodasluneprincakordpitkaplesohulansvistpitkofintadrazechtickastananukbatohprachjelecramecosikakmentmandlpsoukcedicdupaksmaltnaruccmyrakvaltmesecptacezupanolovoslavadojaknapadodkupkozakzradalavkalabutkomarsamotnalevnaletoboranalezbohemdevkasmulaokenamustrcesnorcenihurdazivakvlahastretspionsmrnczabalrazbapalichanzarakevarenasamanbiblenavalbekotvladadoupebodecmilkapriorzabarsokoltoustskvalsuflemakrootrokdativbobekruskotetkaskodaruskarohozobvazrejdaskyvarybkacurakvykalemojivyletmekotvartamarodpizmobolenpedeltackyobrystristturbopismoturbanomadohledmerkasudbakosikcidicvykazkosirveksltrhaclevakvrtacobvodtrhansaltopsotalackabidetsocanubociduetokefirvyskaanexelodarpemzarevirpompaohrevbuzekvolbauzenecouraplenavolbyhrachuzenihrbetpilirmlakakusnaklestodlovodletlankoodhodoponarostisusakvydrazrnkoryzecvysekbankavysecmumiepajanrajkavydrzvysevdotazzapadleteckoupezapalpecenpiniezapasbizonpiluspapezcerenislamvorazbalonkonevodhadvorarbosonsuplepestosenatkoneclickovizummalermagielkanikunkaninjamiozagumakkorbaportofosnavyrazbleskranecletkaportahrnekstrupgenomhrnecfounakaselchlumudolihalernytekhousepuvoddosekkajakfrgalhoustbereczasahjimkasprezuhlirnozirbrblavyukapocitvrtakstejkastmausilibrcalprknopocinuhlikzbozivcelavikevdrinksenorpenzesepselesiklemursosnaamperpisektlupaturnekostecelermajorsynekvagonsnahamamutcenikholubtatarcvikrradloceledvesaksepotcelekpablbkrimiblanaroryscifraasramvegetsazkasatenatasemrzaktonusnormahoteltesartesakschodbasakobrnahledicepeljupkacepecbridzsatekmocalmoukactvrttridalytkozelendedickutilvanekkoalauchylzvastmytnekrcmatuhykbrachkanclhajekkrtekusvitchvattryskprvekvirusnezitvyronmodelsplavmlazivyroktorusflitrdzinysafirvalerorlojakrylcirokvyplnrifletukanfutroplsikotavaplechambitzumbacejkapametetikarandekleniudanimajakutlumpotahodvozpotacnarezrehektaxiksopelpotazcystapotasnarekchrommuldahukotvolekvolejchmelustuplejnoplemehrabechvojprdelharfarevmafenikkurvavlohanarysbulkagoudadlasksykottocnahlozipovykhoubaoddilmlsekloukaporadjoudabourekorekkorenpiratbudkaaspikpopudmodrotuzkahernanahacculikromankopiecyniksadarpevecsedlofenekunavastisktecnaslotapravovpichsakalperonkockavazbadudektemnopalkajichahelmasisalfuserskreksisakbrichskrecfiltrmaserfyzikslipyracekvycuccipsyhaldavrkocdurazmilirdohadmrdkazvuledryakduralvrzalkesonakantchybacentrhnidademonpetakanodazalarmelirdanekzumpaovacechasalijakrohaczubarmadlovyvojtacekgrantfrazetonfaklakavyvozfuriebastagrandestervejcejahlaestetselmaingotdeskybonusknourstacepuskatikettrmendeskarabindareksalvaditkozastirodicpnutivisenbutyluplavtrnozsumarpickasipkakolazpaterrynekcockaguanokolarkapsalezakkokosspajzkokotznenipatekkolacvrasacundrhavirkrachdilnaobtahtabakkeckalilienamelburicskalahltansetbavandrjasnotavbamordamykacprvokpouperetorpilkajadrobukacsluhataskachuzepasazsunardavkafagothobradonnavestadiskojipkapatkaslokazolikzavozfackaloudapinglzavojdohrazavodtrafokodexovsikeshopbosakveznysmetislastsedmabosazlelekdusnoovsirrajceplebsmungorijenbuzikhlistsantaplotrslamapadakvytkarafekdramapapucagavehasakjatralyskabezectrdlokvotavezirsaperjesenpodejfalusukrytrvaniparezdukatborekpapirzavalruckadukazzlunaharemterkafiletdrmekhyzdeusmevrvounserifhrmotselakdeltazvratserikvypadzlostsvenkmalirfrnakvyparspasavznetmalikzokejtahlokahanpugetkurkabelmosepiefrmolsitarholkablahonozkageckosifraneckyhyenazabrarytirkombikrvakjehneberylvranarubaspeckopeckarazicpomocrazieporcekulnadobehsutrazavsitubusgulagnorkaborkajamkagulasmixazpitvaenzymvreniceskohadacpenizhadakorbitceskaholenkarmadezensoupefarmastrehkalupkanalmosuskaluzvybojtotemstredkumstoktetvyborkanarordalstresmeltastrepjasotmorekbaroksosakbaroncurdatchanstavautlakfreonpazbaatlassuitarajdavojinpecetobjevzavitzdvizzazehvhledobjemmiskazdvihdynkolyzarhororajtakmagiknebytodpisodbertonazornatvylovvymazmenzazarezorisehonakrepkahranidcerahranablitiindexdutkafirmaantonvalekpolepvalecfunuszadekskoladutkymisenmiseklepraryteckadetkaderkontolapacvinarzaketzdaniryskakristroceklapakoltarponikdokarmetrovdovaoblekpohupdymkauzemiderbysalekucenipesakburanouskobrimeulitazlazalupekchlevohlaslupennametzizennamermotorpesarchlebcihalneladvycetvycepdoubinugethydrakundasvagrhumussnuravagusdrzkaostenhubkabrylelatkadojemzivelnapevvyverbudikostepbufetbudickrysaobletdusotlazenstychluxuspozarkuzelsalamsalaskozeldoliksojkacubkasekelzivecmoarezlobrzavrtzaceklinkajicenslovolavordulekvravatouhatumortalarkudlazlatobriarpalecfuskapatokkarasuchacpaniczlatynusekutiskmozolspanislivamaskahnutistrukvchodrobotzaberdobroskruzpietabatatbodakzabectlenibojkameterlanyzsperkmrkevsroubsmejdmapkatembrobrussrsenrubinhobbypixelobrucvyvinuklidmokropisarsrnkaratanudicesacekpakazlotosheslocidervynoscucekohavahnitidarcezanetslidarakoskotvalecbavodaknudlebananzidleopukachaosmenicseverrickamyskaburkasirakvasensiranjerabmysakhrudifranktempotretaklimahrbacgrazllalokstrdiryzakmusleroutamezekpahyllexemrafieletakdrkotkonakbabkakrytiemzakkoulehnedovyhraradekpranieidamstartherkavrsektuzbasalbasadlocuvaclumenbidonulicezaricledenuponaledekdopadtravafobienitrohavetargottokajgibonmuletargonlistizvedyelitatrznebotoxlistaubytedasenpoldapajkatrefalovecvzlyktrikokrutaspurtcimravezakbivakdosahtezarvykuphalasropotsufixmogulzaseksmrcipareklysectipecbetelrotorarestparkypraxekojottchorotiskshlukhvizdparkaekzemsinglzabehfonemtapirhazeckulikrigolbordochrpaxeroxodraztajnysedanlobbymedaksedakstuhaumeraosinahudbanavrhplevahnatafalesskunkonucebrzdabacilkurienoumaholictarifturekstaflempirhafanpaskarokleperlalulkazobaksycekneduhdacansasekbasenjehlaoskrtlibrabasetnoteschyseucivokrmicpianospicecachrtlachropakaferavjezdodtahpivkokvartpodilboricsilakzatahkvarkpodivsinushejnosneniorgantunelgymplsvatytopazkarkasvatasypkatucettunerhrochbulvamadalmadamarsenjizvakoreckibicmorcestetibahnofotkakejdabicikmuskaatikakakaovlhkoblatociskavyponebolaklopaemailblataavizovzdorbouraprozaovocelegievlnkakukuclaskazasypbiricvalbadatlevyspaaukcedzudonozikjizdaodenilyzolalkanlamackrasabronzrizekdehetvodicdenimdenikzbranvodiksprascirkaklejtsmichputnalymfaokolivyfukmafiekmotrnazkapolkazdrojdraniposuktraktfararkopakliskabedaratestpoterdrticzinekpotehnaterfazolhusarvickalaserdedekumorimacekvickochmyrpoustatakatrablhusakdomovvolicpannamazalsrankmazaksranidablemlsalmazacopruzprosomotivtenordebettahakpartahudecochozparteasantradkaslevacernooslikcipekcmouddietakabeldrinakoryskopecmudramudrcizmussaunasprejhekalpedalchlupsodikauditulevamisiealbinpolypzlobadinarzakysfetakaudioragbychramfenixnalozdekornemocskransiselsprymnanossextalinieiluzedipollupicvykonvzkazskrytrapiktrudaodnosrapirrezakcestapumpanickamitrarezacserummyckazahybjolkavedropirkocikanokrajmasivcedarpovozsoboltestorotnydojicporodboudacumaksklonosenizdobazajemlikerkodernavodzaletoporatlapazanikvaflesivensazbabystauzlabnakyphajzlnaborvatratroudhysekvykuknadordekansrustnabojserpahanbatuchapanevokleppsicedevcepulpapoetaerozebdenipastakredosrnecpanelvzporupletdudakandelperkotrnkarodeoovcingrupasumecskretzmijekamnaukrokjezekdotykbolidpolakukropsunkasockapoutacarkaodkoppoutolidarsumysmoherricinbutanvichrvolhastikavaricvyhonbranakrovimsicekalpamecikastramargozemakkartazemanzakazsilazmistrrumbarybarlimitdrbnamistokridalepekbedrakecupzebraepikapilaretapazrzkabalsatwistbloudzakerkorzobustazebronacekfolieposunterorcupotopicecteniodkapodkazhonbapsanisemisfluorvyvrtbytnakobravedmaloketzrzekmoudiklasakolejkoleklezecpilatpupikkaucerankatrimrzenitpacka';
+
+const allWordsArr=[];
+let a = "";
+for(var i=0; i<allWordsText.length; i++){
+    if(a.length==5){
+        allWordsArr.push(a);
+        a="";
+    }
+    else{
+        a+=allWordsText[i];
+    }
+}
+
+
+
+const equivalentLetters = {
+    'a': ['a', 'á'],
+    'b': ['b'],
+    'c': ['c', 'č'],
+    'd': ['d', 'ď'],
+    'e': ['e', 'ě', 'é'],
+    'f': ['f'],
+    'g': ['g'],
+    'h': ['h'],
+    'i': ['i', 'í'],
+    'j': ['j'],
+    'k': ['k'],
+    'l': ['l'],
+    'm': ['m'],
+    'n': ['n', 'ň'],
+    'o': ['o', 'ó'],
+    'p': ['p'],
+    'q': ['q'],
+    'r': ['r', 'ř'],
+    's': ['s', 'š'],
+    't': ['t', 'ť'],
+    'u': ['u', 'ú', 'ů'],
+    'v': ['v'],
+    'w': ['w'],
+    'x': ['x'],
+    'y': ['y', 'ý'],
+    'z': ['z', 'ž'],
+};
+
+const letters = ["a", "á", "b", "c", "č", "d", "ď", "e", "é", "ě", "f", "g", "h", "ch", "i", "í", "j", "k", "l", "m", "n", "ň", "o", "ó", "p", "q", "r", "ř", "s", "š", "t", "ť", "u", "ú", "ů", "v", "w", "x", "y", "ý", "z", "ž"];
+
+const state = {
+    secret: "panda", // allWordsArr[Math.floor(Math.random()*allWordsArr.length)],
+    grid: Array(6)
+        .fill()
+        .map(() => Array(5).fill('')),
+    currentRow: 0,
+    currentCol: 0
+};
+
+
+
+function genBox(container, row, col, letter = ''){
+    // create box function
+    const box = document.createElement("div");
+    box.className = "box";
+    box.id = `box${row}${col}`
+    box.textContent = letter;
+
+    container.appendChild(box);
+    return box;
+}
+
+function genGrid(container){
+    // create grid
+    const grid = document.createElement("div");
+    grid.className = "grid";
+
+    // create boxes
+    for(var i = 0; i<6;i++){
+        for(var j = 0; j<5;j++){
+            genBox(grid, i, j);
+        }
+    }
+    // append grid (boxes)
+    container.appendChild(grid)
+}
+
+function refreshGrid(){
+    for(var i=0;i<state.grid.length;i++){
+        for(var j=0;j<state.grid[i].length;j++){
+            const box = document.querySelector(`#box${i}${j}`);
+            box.textContent = state.grid[i][j];
+        }
+    }
+}
+
+
+
+
+function test(){
+    for(var i=0;i < (state.grid.length-1);i++){
+        for(var j=0;i < state.grid[i].length;j++){
+            const box = document.querySelector(`#box${i}${j}`);
+            console.log(box)
+            //box.textContent = state.grid[i][j];
+            
+        }
+    }
+}
+
+function start(){
+    const board = document.querySelector(".board");
+    genGrid(board);
+    registerKey()
+}
+
+function isLetter(key) {
+    return key.length === 1 && key.match(/[a-z]/i);
+}
+
+function registerKey(){
+    document.body.onkeydown = (e) =>{
+        const key = e.key;
+        if(key == "Enter"){
+            if(state.currentCol == 5){
+                const word = getCurrentWord();
+                // if(allWordsArr.includes(word)){
+                if(wordOK){
+                    revealWord(word);
+                    state.currentRow++;
+                    state.currentCol = 0;
+                }
+                else{
+                    window.alert("Tohle slovo neznám.")
+                }
+            }
+
+        }
+        if(key == "Backspace"){
+            removeLetter();
+        }
+        if(key in equivalentLetters){
+            addLetter(key);
+            
+        }
+        refreshGrid();
+        // test()
+    };
+}
+
+function getCurrentWord() {
+    return state.grid[state.currentRow].reduce((prev, curr) => prev + curr);
+}
+
+function addLetter(letter){
+    if(state.currentCol == 5) return;
+    state.grid[state.currentRow][state.currentCol] = letter;
+    state.currentCol++;
+}
+
+function removeLetter(letter){
+    if(state.currentCol == 0) return;
+    state.grid[state.currentRow][state.currentCol - 1] = "";
+    state.currentCol--;
+}
+
+function revealWord(guess){
+    const row = state.currentRow;
+    for(var i=0;i<5;i++){
+        const box = document.querySelector(`#box${row}${i}`);
+        const letter = box.textContent;
+        //console.log(letter)
+        if(letter == state.secret[i]){
+            box.classList.add("right");
+        }
+        else if(state.secret.includes(letter)){
+            box.classList.add("wrong");
+        }
+        else{
+            box.classList.add("empty");
+        }
+    }
+    const isWinner = state.secret == guess;
+    const isGameOver = state.currentRow == 5;
+
+    if(isWinner){
+        window.alert("You won!")
+    }
+    else if(isGameOver){
+        window.alert(`You lost. The word was ${state.secreat}`);
+    }
+}
+
+
+start();
+
+
+function wordOK(w) {
+    var pos = 0;
+    while (1) {
+        pos = allWords.indexOf(w, pos);
+        if (pos < 0)
+            break;
+        if (pos % 5 == 0)
+            return true;
+        pos += 1;
+    }
+    srvLog('w', w);
+    return false;
+}
+
